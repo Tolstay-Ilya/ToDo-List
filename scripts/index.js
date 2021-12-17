04ac5dcf6f65f23932a5faea16738e326ff80441
Ilya Tolstay <kollpachellolo@mail.ru>
2021-12-17 14:56:26 +0300

    perf(ToDo): create TodoItem

--- scripts/index.js
+++ scripts/index.js
@@ -90,3 +90,47 @@ function createHeader() {
     });
     return list;
   }
+
+  function createTodoItem(todo) {
+    const todoItem = createElement(
+      "div",
+      "d-flex align-items-center gap-2 border py-2 px-4 rounded-3"
+    );
+    todoItem.id = todo.id
+    const input = createElement("input", "", "");
+    input.type = "checkbox";
+    input.checked = todo.isCompleted
+    const text = createElement("p", "m-0 flex-grow-1", todo.text);
+  
+    const btnDelete = createElement("button", "btn-close", "");
+  
+    buttonDelete.id = todo.id
+    constdate = createElement("span", "badge bg-secondary mx-2", todo.date)
+   todoItem.addEventListener("input", () =>> {
+     if (input.checked) {
+       todo.isCompleted = true
+       completedTasks++
+       todoItem.className = classInputOn
+       renderTodos(todos)
+     } else {
+       todo.isCompleted = false
+       completedTasks--
+       todoItem.className = classInputOff
+       renderTodos(todos)
+     }
+   })
+   buttonDelete.addEventListener("click", () =>> {
+     const arr = todos.filter((event) => event.id !== todo.id)
+     if (todo.isCompleted === true) {
+       allTasks--
+       completedTasks--
+       todos = arr
+       renderTodos(todos)
+       return todos
+     } else {
+       allTasks--
+       todos = arr
+       renderTodos(todos)
+       return arr
+     }
+   })
