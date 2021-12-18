a64055b490607c3d5a7e528d03732af1b98692f2
Ilya Tolstay <kollpachellolo@mail.ru>
2021-12-19 00:47:46 +0300

    feat(ToDo): add masive

---
+++ script.js
@@ -0,0 +1,27 @@
+const todos = [
+    {
+      id: 1,
+      text: "Lorem",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Cooking",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Drink water",
+      isCompleted: false,
+    },
+    {
+      id: 4,
+      text: "Play PC",
+      isCompleted: false,
+    },
+    {
+      id: 5,
+      text: "Reading books",
+      isCompleted: false,
+    },
+  ];
